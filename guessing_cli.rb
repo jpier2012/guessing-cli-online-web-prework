cab0546816e1ee0699136485ee785eb995f668ce
James Pier <jpier2012@gmail.com>
2019-04-12 13:20:27 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,14 +1,5 @@
 # Code your solution here!
 # unless a winning message is passed, it will print the default incorrect input notification
-
-def message(msg = "Incorrect input, please try again.")
-  msg
-end
-
-###########
-###########
-
-
 def take_input
   puts "Guess a number between 1 and 6."
   input = gets.chomp.to_i
