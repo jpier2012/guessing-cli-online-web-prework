0f5c59375f5dae7ca6a35ce4f5860687c4f34938
James Pier <jpier2012@gmail.com>
2019-04-12 13:19:04 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,5 +1,19 @@
 # Code your solution here!
+def message(msg = "Incorrect input, please try again.")
 
+  msg
+end
+
+def take_input
+  message = "Incorrect input, please try again."
+  puts "Guess a number between 1 and 6."
+  input = gets.chomp.to_i
+
+  
+  if 
+
+  puts 
+end
 
 
 
@@ -9,5 +23,5 @@
 ###########
 
 def run_guessing_game
-
+  
 end
