29bb7b6d0884be3e62d3aed652a52eec3930eacb
James Pier <jpier2012@gmail.com>
2019-04-12 13:23:37 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,27 +1,15 @@
 # Code your solution here!
 # unless a winning message is passed, it will print the default incorrect input notification
-def take_input
+def run_guessing_game
   num = rand(1..6)
   puts "Guess a number between 1 and 6."
   input = gets.chomp.to_i
-
-
+  
   if input == num
     puts "You guessed the correct number!"
   else
     puts "The com"
-
-
-  puts message()
 end
-
-
-
-
 
 ###########
 ###########
-
-def run_guessing_game
-
-end
